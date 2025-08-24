8112a62028f6d943dde76d79e2c34d2b9f266688
beherit197777 <beherit197777@gmail.com>
2025-08-24 14:50:18 +0300

    add message migration

---
+++ database/migrations/2025_08_24_114529_create_messages_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('repost')->default(false);
+            $table->unsignedBigInteger('answer')->nullable()->index();
+            $table->string('author')->nullable();
+            $table->text('body');
+            $table->unsignedBigInteger('chat')->index();
+            $table->timestamps();
+
+            $table->foreign('chat')->references('id')->on('chats')->onDelete('cascade');
+            $table->foreign('answer')->references('id')->on('messages')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
