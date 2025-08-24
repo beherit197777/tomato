b3a9ba76360be9b03f06a50aaad1704566fe7be9
beherit197777 <beherit197777@gmail.com>
2025-08-24 14:42:21 +0300

    add Comments migration

---
+++ app/Console/Commands/GoCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Post;
+class GoCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'go';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Post::create([
+            'title' => 'My awesome post'
+        ]);
+    }
+}
